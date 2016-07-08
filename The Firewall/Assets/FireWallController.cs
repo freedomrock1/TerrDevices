fb5f0635c13e0f32d282bc0f79350874f4295d9d
Harrison John Hopkins <hguyhopkins@gmail.com>
2016-07-07 22:02:10 -0400

    Added Firewall
    Approach the Firewall and click with the Left mouse button to be taken
    to the scene with a close-up of the firewall. When in the scene facing
    the firewall, hit the "R" Key to return to the previous scene.

---
+++ The Firewall/Assets/FireWallController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FireWallController : MonoBehaviour
+{
+
+    public bool isClose = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.visible = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (isClose && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Firewall");
+        }
+
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isClose = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isClose = false;
+        }
+    }
+}
