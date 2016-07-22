19a790d401352a136ad29be9e302507ef425ec8e
Harrison John Hopkins <hguyhopkins@gmail.com>
2016-07-21 21:18:41 -0400

    Added Router Travel
    When you approach the Router and get within a short distance of it, you
    can Left Click to travel to a new scene with a Router. Hitting the "R"
    key will return you to the FPS scene.

---
+++ The Firewall/Assets/RouterController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RouterController : MonoBehaviour
+{
+
+    public bool isClose = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.visible = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	    if(isClose && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Router");
+        }
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isClose = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isClose = false;
+        }
+    }
+
+
+}
