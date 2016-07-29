3cd5bbe673ec6fed6491b4dfecf6a37e87bde9d5
Harrison John Hopkins <hguyhopkins@gmail.com>
2016-07-28 20:15:16 -0400

    Added Workstation Scene
    Same as previous Scenes. Approaching the Workstation and Left-Clicking
    anywhere will transport the player to a Workstation Scene. Hitting the R
    button will return the player to the FPS scene.

---
+++ The Firewall/Assets/Workstation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Workstation : MonoBehaviour
+{
+
+    public bool isClose = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(isClose && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Workstation");
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isClose = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isClose = false;
+        }
+    }
+}
